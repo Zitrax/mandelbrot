b2341818993fe279bc74c0af0cd50bb5cc3dfe38
Daniel Bengtsson <daniel@bengtssons.info>
2019-10-04 23:26:03 +0200

    Playing around further

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -5,6 +5,8 @@
 
 #include <raylib.h>
 
+using namespace std::complex_literals;
+
 using ctype = float;
 using cmp = std::complex<ctype>;
 
@@ -14,24 +16,23 @@ auto mandel(cmp z, cmp c)
     return std::pow(z, 2) + c;
 }
 
-const int screenWidth = 1400;
-const int screenHeight = 800;
-const int N = 50;
+const int screenWidth = 1280;
+const int screenHeight = 960;
+const uint8_t N = 250;
 
 // Check if we diverge and are thus in the Mandelbrot set
-unsigned char mandelRes(cmp c)
+uint8_t mandelRes(cmp c, cmp z = 0)
 {
-    cmp z = 0;
-    for (int n = 0; n < N; ++n)
+    for (uint8_t n = 0; n < N; ++n)
     {
         z = mandel(z, c);
-        if(std::abs(z) > 2) { // Explodes
-            return 256 * n/N;
+        if (std::abs(z) > 2)
+        { // Explodes
+            return std::numeric_limits<uint8_t>::max() * n / N;
         }
     }
     return 0;
 }
-
 
 // Scale to [-2, 1]
 ctype scaleX(ctype x)
@@ -45,37 +46,56 @@ ctype scaleY(ctype y)
     return 2 * (y / screenHeight) - 1;
 }
 
-int main()
+auto draw(std::array<uint8_t, screenHeight * screenWidth> &buffer, cmp z = 0)
 {
-    InitWindow(screenWidth, screenHeight, "raylib [shapes] example - basic shapes drawing");
-
-    const int fps = 60;
-    SetTargetFPS(fps);
-
-    auto buffer = std::make_unique<std::array<char,screenHeight*screenWidth>>();
     for (int x = 0; x < screenWidth; ++x)
     {
         for (int y = 0; y < screenHeight; ++y)
         {
-            buffer->at(x + screenWidth * y) = mandelRes(cmp(scaleX(x), scaleY(y)));
+            buffer[x + screenWidth * y] = mandelRes(cmp(scaleX(static_cast<ctype>(x)), scaleY(static_cast<ctype>(y))), z);
         }
     }
+}
 
+int main()
+{
+    InitWindow(screenWidth, screenHeight, "raylib [shapes] example - basic shapes drawing");
+    ToggleFullscreen();
+
+    const int fps = 30;
+    SetTargetFPS(fps);
+
+    auto buffer = std::make_unique<std::array<uint8_t, screenHeight * screenWidth>>();
+    draw(*buffer);
+
+    int frame = 0;
+    int frame2 = 0;
+    int frame3 = 0;
     while (!WindowShouldClose()) // Detect window close button or ESC key
     {
         BeginDrawing();
         ClearBackground(BLACK);
+
+        //if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+            //z += 0.01 + 0i;
+            //draw(*buffer, z);
+        }
 
         for (int x = 0; x < screenWidth; ++x)
         {
             for (int y = 0; y < screenHeight; ++y)
             {
                 unsigned char val = buffer->at(x + screenWidth * y);
-                DrawPixel(x, y, Color{val,val,val,255});
+                DrawPixel(x, y, Color{(unsigned char)(val * frame3 % 256), (unsigned char)(val + frame % 256), (unsigned char)(val - frame2 % 256), 255});
             }
         }
+        frame++;
+        frame2 += 3;
+        frame3 += 7;
 
         DrawText("Mandelbrot set test", 20, 20, 20, DARKGRAY);
+        DrawText(std::to_string(GetFPS()).c_str(), screenWidth - 100, 20, 20, DARKGRAY);
         EndDrawing();
     }
 
