f226c8ad18eb23357cc4b6277f72a1c69b661575
Daniel Bengtsson <daniel@bengtssons.info>
2019-10-05 16:29:02 +0200

    Zoom and navigation

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -8,7 +8,7 @@
 
 using namespace std::complex_literals;
 
-using ctype = float;
+using ctype = double;
 using cmp = std::complex<ctype>;
 
 // f(z) = z^2 + c
@@ -19,7 +19,7 @@ auto mandel(cmp z, cmp c)
 
 const int screenWidth = 1920;
 const int screenHeight = 1280;
-const uint8_t N = 250;
+const uint8_t N = 50;
 
 // Check if we diverge and are thus in the Mandelbrot set
 uint8_t mandelRes(cmp c, cmp z = 0)
@@ -28,23 +28,27 @@ uint8_t mandelRes(cmp c, cmp z = 0)
     {
         z = mandel(z, c);
         if (std::norm(z) > 4) // The algorithm is std::abs(z) > 2, but this is faster
-        { // Explodes
+        {                     // Explodes
             return std::numeric_limits<uint8_t>::max() * n / N;
         }
     }
     return 0;
 }
 
+float x_offset = 0;
+float y_offset = 0;
+float zoom = 1;
+
 // Scale to [-2, 1]
 ctype scaleX(ctype x)
 {
-    return 3 * (x / screenWidth) - 2;
+    return zoom * 3 * (x / screenWidth) - zoom * 2 + x_offset;
 }
 
 // Scale to [-1, 1]
 ctype scaleY(ctype y)
 {
-    return 2 * (y / screenHeight) - 1;
+    return zoom * 2 * (y / screenHeight) - zoom * 1 + y_offset;
 }
 
 auto draw(std::array<uint8_t, screenHeight * screenWidth> &buffer, cmp z = 0)
@@ -60,13 +64,12 @@ auto draw(std::array<uint8_t, screenHeight * screenWidth> &buffer, cmp z = 0)
 int main()
 {
     InitWindow(screenWidth, screenHeight, "raylib [shapes] example - basic shapes drawing");
-    //ToggleFullscreen();
+    ToggleFullscreen();
 
     const int fps = 60;
     SetTargetFPS(fps);
 
     auto buffer = std::make_unique<std::array<uint8_t, screenHeight * screenWidth>>();
-    draw(*buffer);
 
     cmp z = 0;
     int frame = 0;
@@ -75,10 +78,32 @@ int main()
         BeginDrawing();
         ClearBackground(BLACK);
 
-        //if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        if (IsKeyDown(KEY_UP))
         {
-            draw(*buffer, z += 0.0001 + 0i);
+            y_offset -= 0.05f;
         }
+        if (IsKeyDown(KEY_DOWN))
+        {
+            y_offset += 0.05f;
+        }
+        if (IsKeyDown(KEY_RIGHT))
+        {
+            x_offset += 0.05f;
+        }
+        if (IsKeyDown(KEY_LEFT))
+        {
+            x_offset -= 0.05f;
+        }
+        if (IsKeyDown(KEY_KP_ADD))
+        {
+            zoom -= 0.05f;
+        }
+        if (IsKeyDown(KEY_KP_SUBTRACT))
+        {
+            zoom += (2-zoom) * 0.05f;
+        }
+
+        draw(*buffer);      
 
         for (int x = 0; x < screenWidth; ++x)
         {
@@ -88,7 +113,7 @@ int main()
                 DrawPixel(x, y, Color{(unsigned char)(val * frame % 256), (unsigned char)(val + frame % 256), (unsigned char)(val - frame % 256), 255});
             }
         }
-        frame++;
+
 
         DrawText("Mandelbrot set test", 20, 20, 20, DARKGRAY);
         DrawText(std::to_string(GetFPS()).c_str(), screenWidth - 100, 20, 20, DARKGRAY);
