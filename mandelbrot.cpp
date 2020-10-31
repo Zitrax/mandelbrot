afd133f837e9f9d1338d6443b61ee9daccc09e56
Daniel Bengtsson <daniel@bengtssons.info>
2020-10-31 20:12:02 +0100

    smoother test

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -18,11 +18,10 @@ auto mandel(cmp z, cmp c)
     return std::pow(z, 2) + c;
 }
 
-constexpr int screenWidth = 1024;
-constexpr int screenHeight = 768;
+constexpr int screenWidth = 1024*2;
+constexpr int screenHeight = 768*2;
 constexpr uint8_t N = 50;
 using screen = std::array<uint8_t, screenHeight * screenWidth>;
-
 
 // Check if we diverge and are thus in the Mandelbrot set
 uint8_t mandelRes(cmp c, cmp z = 0)
@@ -38,9 +37,9 @@ uint8_t mandelRes(cmp c, cmp z = 0)
     return 0;
 }
 
-float x_offset = 0;
-float y_offset = 0;
-float zoom = 1;
+ctype x_offset = 0;
+ctype y_offset = 0;
+ctype zoom = 1;
 
 // Scale to [-2, 1]
 ctype scaleX(ctype x)
@@ -56,7 +55,7 @@ ctype scaleY(ctype y)
 
 auto calc(screen &buffer, cmp z = 0)
 {
-    std::transform(std::execution::par, buffer.begin(), buffer.end(), buffer.begin(), [&](auto &p) {
+    std::transform(std::execution::par_unseq, buffer.begin(), buffer.end(), buffer.begin(), [&](auto &p) {
         const size_t i = &p - &buffer[0];
         const auto x = i % screenWidth;
         const auto y = i / screenHeight;
@@ -64,86 +63,88 @@ auto calc(screen &buffer, cmp z = 0)
     });
 }
 
-void draw(int xp, int yp, int w, int h, int frame, const screen& buffer)
+void draw(int xp, int yp, int w, int h, int frame, const screen &buffer)
 {
-  for (int x = xp; x < xp+w; ++x) {
-    for (int y = yp; y < yp+h; ++y) {
-      unsigned char val = buffer.at(x + screenWidth * y);
-      DrawPixel(x, y, Color{(unsigned char)(val * frame % 256),
-                            (unsigned char)(val + frame % 256),
-                            (unsigned char)(val - frame % 256), 255});
+    for (int x = xp; x < xp + w; ++x)
+    {
+        for (int y = yp; y < yp + h; ++y)
+        {
+            unsigned char val = buffer.at(x + screenWidth * y);
+            DrawPixel(x, y, Color{(unsigned char)(val * frame % 256), (unsigned char)(val + frame % 256), (unsigned char)(val - frame % 256), 255});
+        }
     }
-  }
 }
 
 int main()
 {
     InitWindow(screenWidth, screenHeight, "Mandelbrot testing");
-    //ToggleFullscreen();
+    ToggleFullscreen();
 
-  constexpr int fps = 60;
-  SetTargetFPS(fps);
+    constexpr int fps = 60;
+    SetTargetFPS(fps);
 
-  auto buffer = std::make_unique<screen>();
+    auto buffer = std::make_unique<screen>();
 
-  // cmp z = 0;
-  bool first = true;
-  int frame = 0;
-  int zoom_step = 1;
-  bool changed = true;
-  ClearBackground(BLACK);
-  while (!WindowShouldClose()) // Detect window close button or ESC key
-  {
-    changed = true;
-    if (IsKeyDown(KEY_UP))
+    // cmp z = 0;
+    bool first = true;
+    int frame = 0;
+    bool changed = true;
+    ClearBackground(BLACK);
+    while (!WindowShouldClose()) // Detect window close button or ESC key
     {
-      y_offset -= zoom*0.05f;
-    }
-    else if (IsKeyDown(KEY_DOWN))
-    {
-      y_offset += zoom*0.05f;
-    }
-    else if (IsKeyDown(KEY_RIGHT))
-    {
-      x_offset += zoom*0.05f;
-    }
-    else if (IsKeyDown(KEY_LEFT))
-    {
-      x_offset -= zoom*0.05f;
-    }
-    else if (IsKeyDown(KEY_KP_ADD))
-    {
-      zoom_step++;
-      zoom = 1.0/zoom_step;
-    }
-    else if (IsKeyDown(KEY_KP_SUBTRACT))
-    {
-      if(zoom_step>1)
-        zoom_step--;
-      zoom = 1.0/zoom_step;
-    }
-    else if(!first)
-    {
-      changed = false;
-    }
-    first = false;
+        changed = true;
+        if (IsKeyDown(KEY_UP))
+        {
+            y_offset -= zoom * 0.05f;
+        }
+        else if (IsKeyDown(KEY_DOWN))
+        {
+            y_offset += zoom * 0.05f;
+        }
+        else if (IsKeyDown(KEY_RIGHT))
+        {
+            x_offset += zoom * 0.05f;
+        }
+        else if (IsKeyDown(KEY_LEFT))
+        {
+            x_offset -= zoom * 0.05f;
+        }
+        else if (IsKeyDown(KEY_KP_ADD))
+        {
+            zoom /= 1.02;
+        }
+        else if (IsKeyDown(KEY_KP_SUBTRACT))
+        {
+            zoom *= 1.02;
+        }
+        else if (!first)
+        {
+            changed = false;
+        }
+        first = false;
 
-    BeginDrawing();
-    if (changed)
-    {
-      calc(*buffer);
-      draw(0, 0, screenWidth, screenHeight, frame, *buffer);
-      DrawText("Mandelbrot set test", 20, 20, 20, DARKGRAY);
-    } else {
-      draw(screenWidth-100, 20, 100, 20, frame, *buffer);
+        BeginDrawing();
+        if (changed)
+        {
+            calc(*buffer);
+            draw(0, 0, screenWidth, screenHeight, frame, *buffer);
+            DrawText("Mandelbrot set test", 20, 20, 20, DARKGRAY);
+        }
+        else
+        {
+#ifdef _WIN32
+            // For some reason we need to draw all on win, but not lin or we flicker
+            draw(0, 0, screenWidth, screenHeight, frame, *buffer);
+#else
+            draw(screenWidth - 100, 20, 100, 20, frame, *buffer);
+#endif
+        }
+
+        DrawText(std::to_string(GetFPS()).c_str(), screenWidth - 100, 20, 20, DARKGRAY);
+        EndDrawing();
     }
-    
-    DrawText(std::to_string(GetFPS()).c_str(), screenWidth - 100, 20, 20, DARKGRAY);
-    //DrawText(std::to_string(zoom).c_str(), screenWidth - 100, 20, 20, DARKGRAY);
-    EndDrawing();
-  }
 
-  CloseWindow(); // Close window and OpenGL context
+    CloseWindow(); // Close window and OpenGL context
 
-  return 0;
+    return 0;
 }
